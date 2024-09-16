9a54f89d0ba4f246fd192d8c1677cb6ae261e5bf
Estefanía García Gallardo <nyablk97@gmail.com>
2024-09-16 13:16:54 +0200

    feat(input): update input component

--- src/grammar/input/input.component.ts
+++ src/grammar/input/input.component.ts
@@ -2,14 +2,22 @@ import { Component } from '@angular/core';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
+import {FormControl, ReactiveFormsModule} from '@angular/forms';
+import {MatButtonModule} from '@angular/material/button';
+
 
 @Component({
   selector: 'app-input',
   standalone: true,
-  imports: [MatFormFieldModule, MatInputModule, MatIconModule],
+  imports: [MatFormFieldModule, MatInputModule, MatIconModule, ReactiveFormsModule, MatButtonModule],
   templateUrl: './input.component.html',
   styleUrl: './input.component.scss'
 })
 export class InputComponent {
+  grammar: FormControl = new FormControl("");
 
+  public validateGrammar():void {
+    const grammarValue: String = this.grammar.value;
+    console.log(grammarValue);
+  }
 }
