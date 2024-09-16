ff74dfa4d5287518fbd118e05edf3c87b23258bd
Estefanía García Gallardo <nyablk97@gmail.com>
2024-09-16 13:17:35 +0200

    feat(grammar): add file-upload component

--- src/grammar/grammar/grammar.component.ts
+++ src/grammar/grammar/grammar.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { InputComponent } from '../input/input.component';
 import { HelpDropdownComponent } from '../help-dropdown/help-dropdown.component';
+import { FileUploadComponent } from '../file-upload/file-upload.component';
 
 @Component({
   selector: 'app-grammar',
   standalone: true,
-  imports: [ InputComponent, HelpDropdownComponent],
+  imports: [ InputComponent, HelpDropdownComponent, FileUploadComponent],
   templateUrl: './grammar.component.html',
   styleUrl: './grammar.component.scss'
 })
