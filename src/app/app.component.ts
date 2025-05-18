bacb901a5edd539457765a59e8444a7322e9a7cf
Nya García <nyablk97@gmail.com>
2025-05-18 13:01:02 +0200

    fix(app.component): styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { GrammarComponent } from '../grammar/components/grammar/grammar.component';
 import { TreeComponent } from './tree/components/tree/tree.component';
 import { CommonModule } from '@angular/common';
@@ -15,6 +15,8 @@ import {
 } from '../grammar/interfaces/production-rule.interface';
 import { EPSILON } from './utils/constants';
 import { GrammarValidatorService } from '../grammar/services/grammar-validator.service';
+import { MatDialog } from '@angular/material/dialog';
+import { ErrorModalComponent } from '../grammar/components/error-modal/error-modal.component';
 
 @Component({
   selector: 'app-root',
@@ -39,7 +41,7 @@ export class AppComponent {
   displayTree: boolean = false;
 
   validationErrors: string[] = [];
-  showModal = false;
+  dialog = inject(MatDialog);
 
   constructor(
     public darkModeService: DarkModeService,
@@ -56,7 +58,6 @@ export class AppComponent {
   }
 
   loadFile(file: File) {
-    this.selectedTabIndex = 0;
     this.displayTree = false;
 
     const reader = new FileReader();
@@ -83,8 +84,9 @@ export class AppComponent {
 
         if (validationResult.errors.length > 0) {
           this.validationErrors = validationResult.errors;
-          this.showModal = true;
+          this.displayDialog(this.validationErrors);
         } else {
+          this.selectedTabIndex = 0;
           this.loadedGrammar = validationResult.unformattedGrammar;
         }
       } catch (err) {
@@ -99,64 +101,11 @@ export class AppComponent {
     reader.readAsText(file);
   }
 
-  private validateJson(raw: UnformattedGrammar) {
-    if (
-      !raw ||
-      !Array.isArray(raw.terminals) ||
-      !Array.isArray(raw.nonTerminals) ||
-      !Array.isArray(raw.productionRules)
-    ) {
-      throw new Error(
-        'Invalid grammar structure: Missing terminals, nonTerminals, or productionRules.'
-      );
-    }
-
-    const { terminals, nonTerminals, productionRules } = raw;
-
-    const validSymbols = new Set([...terminals, ...nonTerminals, '']);
-
-    const processedRules: ProductionRule[] = productionRules.map(
-      (rule: ProductionRule, index: number) => {
-        const left = rule.leftProductionRule;
-
-        if (typeof left !== 'string' || left.trim() === '') {
-          throw new Error(
-            `Rule ${index}: leftProductionRule must be a non-empty string.`
-          );
-        }
-
-        if (!nonTerminals.includes(left)) {
-          throw new Error(
-            `Rule ${index}: leftProductionRule "${left}" is not in nonTerminals.`
-          );
-        }
-
-        if (!Array.isArray(rule.rightProductionRule)) {
-          throw new Error(
-            `Rule ${index}: rightProductionRule must be an array.`
-          );
-        }
-
-        const updatedRight = rule.rightProductionRule.map((symbol, i) => {
-          if (!validSymbols.has(symbol)) {
-            throw new Error(
-              `Rule ${index}: Symbol "${symbol}" at position ${i} is not declared.`
-            );
-          }
-          return symbol === '' ? EPSILON : symbol;
-        });
-
-        return {
-          leftProductionRule: left,
-          rightProductionRule: updatedRight,
-        };
-      }
-    );
-
-    return {
-      terminals,
-      nonTerminals,
-      productionRules: processedRules,
-    };
+  private displayDialog(errors: string[]) {
+    this.dialog.open(ErrorModalComponent, {
+      data: {
+        errors,
+      },
+    });
   }
 }
