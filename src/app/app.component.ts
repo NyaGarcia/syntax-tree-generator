116a7c0896a4077cd559be114fab36b032d60be3
Nya García <nyablk97@gmail.com>
2025-05-21 16:53:21 +0200

    fix(app.component): avoid recomputing signal in template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -43,6 +43,8 @@ export class AppComponent {
   validationErrors: string[] = [];
   dialog = inject(MatDialog);
 
+  darkMode = this.darkModeService.darkModeSignal();
+
   constructor(
     public darkModeService: DarkModeService,
     private grammarValidatorService: GrammarValidatorService
